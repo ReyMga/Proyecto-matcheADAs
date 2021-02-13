6b46ad23d29991a0794b9225b0f9410e1a1d0534
ReyMga <muriega.raquel@gmail.com>
2021-02-13 17:53:06 -0300

    fixed setInterval fuction

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict'
 
 //Crear tablas dinámicas
-var $ = function( id ) { return document.getElementById( id ); }
+var $ = function (id) {
+    return document.getElementById(id);
+}
 const gridContainer = document.getElementById('gridContainer');
 let selectedCell = null;
 
 function generaTabla(cantFilas) {
     window.modal.closeCurrent()
+    counterInitialitation();
     // Obtener la referencia del elemento body
     const body = document.getElementsByTagName("body")[0];
 
@@ -16,22 +19,22 @@ function generaTabla(cantFilas) {
 
     // Creo las celdas
     for (let i = 0; i < cantFilas; i++) {
-     
+
         const hilera = document.createElement("tr");
         for (var j = 0; j < cantFilas; j++) {
-          
+
             const celda = document.createElement("td");
-            celda.id = 'cell_'+i+j;
+            celda.id = 'cell_' + i + j;
             celda.row = i;
             celda.column = j;
-            
+
             const textoCelda = document.createTextNode(getRandomSymbol());
             const span = document.createElement("span");
             span.className = 'cell-element';
             span.appendChild(textoCelda);
             celda.appendChild(span);
-            celda.addEventListener('click', ()=>{
-             seleccionarCelda(celda)
+            celda.addEventListener('click', () => {
+                seleccionarCelda(celda)
             })
             hilera.appendChild(celda);
         }
@@ -51,41 +54,41 @@ function getRandomInt(min, max) {
 
 //Funcion para generar iconos random
 function getRandomSymbol() {
-    const number = getRandomInt(0,5);
-    switch (number){
-        case 0: 
-        return "🚀";
-        case 1: 
-        return "👽";
-        case 2: 
-        return "🤖";
-        case 3: 
-        return "🛸";
-        case 4: 
-        return "👾";
+    const number = getRandomInt(0, 5);
+    switch (number) {
+        case 0:
+            return "🚀";
+        case 1:
+            return "👽";
+        case 2:
+            return "🤖";
+        case 3:
+            return "🛸";
+        case 4:
+            return "👾";
     }
 }
 
 //Función para seleccionar celda
 
-function seleccionarCelda(celda){
-    if(!selectedCell){
+function seleccionarCelda(celda) {
+    if (!selectedCell) {
         selectedCell = {
-            row : celda.row, 
-            column : celda.column
+            row: celda.row,
+            column: celda.column
         }
         //celda.style.border='';
         celda.firstElementChild.className = 'cell-element-selected';
-    }else{
-        if(!nextSelectedItem(celda.row,celda.column)){
+    } else {
+        if (!nextSelectedItem(celda.row, celda.column)) {
             //Despinto la seleccionada y pinto el nuevo item seleccionado
-            let oldSelectedCell = $('cell_'+ selectedCell.row + selectedCell.column);
+            let oldSelectedCell = $('cell_' + selectedCell.row + selectedCell.column);
             //oldSelectedCell.style.border = 'none';
             oldSelectedCell.firstElementChild.className = 'cell-element';
             celda.firstElementChild.className = 'cell-element-selected';
             selectedCell = {
-                row : celda.row, 
-                column : celda.column
+                row: celda.row,
+                column: celda.column
             }
         }
 
@@ -93,7 +96,7 @@ function seleccionarCelda(celda){
 }
 
 
-function nextSelectedItem(i,j){
+function nextSelectedItem(i, j) {
     return false;
 }
 
@@ -103,9 +106,12 @@ function nextSelectedItem(i,j){
 
 var n = 0;
 const l = document.getElementById("timeCounter");
-window.setInterval(function(){
-  
 
-  l.innerHTML = '0:' + n.toString().padStart(2, '0');
-  n++;
-},1000);
+function counterInitialitation() {
+    l.innerHTML = '0:' + n.toString().padStart(2, '0');
+    n++;
+    window.setInterval(function () {
+        l.innerHTML = '0:' + n.toString().padStart(2, '0');
+        n++;
+    }, 1000);
+}
